55d9e5d429ef82f1d7b8298f021a41242e024c6b
gergo-m <119097427+gergo-m@users.noreply.github.com>
2022-12-14 21:02:49 +0100

    Changed structure
    moving in tiles instead of drag&drop

--- puzzle/index.js
+++ puzzle/index.js
@@ -5,18 +5,104 @@
 window.addEventListener("resize", resize, false);
 resize();*/
 
-for (let i = 1; i <= 15; i++) {
+/*for (let i = 1; i <= 15; i++) {
     dragElement(document.getElementById("box" + i));
     document.getElementById("box" + i).style.top = String((document.documentElement.clientHeight-374)*Math.random()) + "px";
     document.getElementById("box" + i).style.left = String((document.documentElement.clientWidth-392)*Math.random()) + "px";
-}
+}*/
 
 /*console.log(window.innerHeight, window.innerWidth);
 console.log(visualViewport.height, visualViewport.width)
 console.log(document.documentElement.clientHeight, document.documentElement.clientWidth);*/
 
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-let highestZindex = 0
+for (let i = 1; i <=14; i++) {
+    var rand = Math.floor(Math.random() * 15) + 1;
+    console.log(rand);
+    if (document.getElementById("pl"+i)) {
+        //while (nums[rand] == 0) {
+            var prLastChild = document.getElementById("pl"+i).children[document.getElementById("pl"+i).childElementCount-1];
+            document.getElementById("pl"+i).appendChild(document.getElementById("pl"+rand).children[document.getElementById("pl"+rand).childElementCount-1]);
+            document.getElementById("pl"+rand).appendChild(prLastChild);
+        //    rand = Math.floor(Math.random() * 15);
+        //}
+        //nums[rand] = 0;
+    }
+}
+
+const dirs = ["left", "right", "down", "up"];
+const dirsc = ["-1", "+1", "+3", "-3"];
+
+for (let i = 0; i <=3; i++) {
+    for (let j = 1; j <=15; j++) {
+        /*console.log("1worked");
+        console.log(dirs[i]+j);
+        console.log(dirsc[i]);
+        console.log(j+Number(dirsc[i]));      */                                       // i = 3       j = 14
+        if (document.getElementById(dirs[i] + j)) {
+            document.getElementById(dirs[i] + j).onclick = function() {
+                console.log("2worked");
+                var prLastChild = document.getElementById("pl" + (j+Number(dirsc[i]))).children[document.getElementById("pl" + (j+Number(dirsc[i]))).childElementCount-1];
+                console.log(document.getElementById("pl" + (j)).children[document.getElementById("pl" + (j)).childElementCount-1]);
+                console.log(document.getElementById("pl" + (j+Number(dirsc[i]))).children[document.getElementById("pl" + (j+Number(dirsc[i]))).childElementCount-1]);
+                document.getElementById("pl" + (j+Number(dirsc[i]))).appendChild(document.getElementById("pl" + (j)).children[document.getElementById("pl" + (j)).childElementCount-1]);
+                document.getElementById("pl" + j).appendChild(prLastChild);
+            }
+        }
+    }
+}
+
+/*for (let i = 1; i <= 15; i++) {
+    console.log("1worked")
+    if (document.getElementById("up" + i)) {
+        document.getElementById("up" + i).onclick = function() {
+            console.log("2worked")
+            var spos = document.getElementById("up" + (i-3));
+            var prLastChild = document.getElementById("pl" + (i-3)).children[document.getElementById("pl" + (i-3)).childElementCount-1];
+            console.log(document.getElementById("pl" + (i)).children[document.getElementById("pl" + (i)).childElementCount-1])
+            console.log(document.getElementById("pl" + (i-3)).children[document.getElementById("pl" + (i-3)).childElementCount-1])
+            document.getElementById("pl" + (i-3)).appendChild(document.getElementById("pl" + (i)).children[document.getElementById("pl" + (i)).childElementCount-1]);
+            document.getElementById("pl" + i).appendChild(prLastChild);
+        }
+    }
+    if (document.getElementById("down" + i)) {
+        document.getElementById("down" + i).onclick = function() {
+            console.log("2worked")
+            var spos = document.getElementById("down" + (i+3));
+            var prLastChild = document.getElementById("pl" + (i+3)).children[document.getElementById("pl" + (i+3)).childElementCount-1];
+            console.log(document.getElementById("pl" + (i)).children[document.getElementById("pl" + (i)).childElementCount-1])
+            console.log(document.getElementById("pl" + (i+3)).children[document.getElementById("pl" + (i+3)).childElementCount-1])
+            document.getElementById("pl" + (i+3)).appendChild(document.getElementById("pl" + (i)).children[document.getElementById("pl" + (i)).childElementCount-1]);
+            document.getElementById("pl" + i).appendChild(prLastChild);
+        }
+    }
+    if (document.getElementById("right" + i)) {
+        document.getElementById("right" + i).onclick = function() {
+            console.log("2worked")
+            var spos = document.getElementById("right" + (i+1));
+            var prLastChild = document.getElementById("pl" + (i+1)).children[document.getElementById("pl" + (i+1)).childElementCount-1];
+            console.log(document.getElementById("pl" + (i)).children[document.getElementById("pl" + (i)).childElementCount-1])
+            console.log(document.getElementById("pl" + (i+1)).children[document.getElementById("pl" + (i+1)).childElementCount-1])
+            document.getElementById("pl" + (i+1)).appendChild(document.getElementById("pl" + (i)).children[document.getElementById("pl" + (i)).childElementCount-1]);
+            document.getElementById("pl" + i).appendChild(prLastChild);
+        }
+    }
+    if (document.getElementById("left" + i)) {
+        document.getElementById("left" + i).onclick = function() {
+            console.log("2worked")
+            var spos = document.getElementById("left" + (i-1));
+            var prLastChild = document.getElementById("pl" + (i-1)).children[document.getElementById("pl" + (i-1)).childElementCount-1];
+            console.log(document.getElementById("pl" + (i)).children[document.getElementById("pl" + (i)).childElementCount-1])
+            console.log(document.getElementById("pl" + (i-1)).children[document.getElementById("pl" + (i-1)).childElementCount-1])
+            document.getElementById("pl" + (i-1)).appendChild(document.getElementById("pl" + (i)).children[document.getElementById("pl" + (i)).childElementCount-1]);
+            document.getElementById("pl" + i).appendChild(prLastChild);
+        }
+    }
+}*/
+
+
+/*let highestZindex = 0
 
 function dragElement(element) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -73,7 +159,7 @@ window.onload = function() {
     every time it fires, grab the location
     of touch and assign it to box */
 
-    function touchEvent(element) {
+    /*function touchEvent(element) {
         element.addEventListener("touchstart", function(e) {
             pos3 = e.targetTouches[0].clientX;
             pos4 = e.targetTouches[0].clientY;
@@ -94,4 +180,4 @@ window.onload = function() {
             element.style.left = roundToNum(93.5, element.offsetLeft - pos1) + "px";
         });
     }    
-  }
+  }*/
