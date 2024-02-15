4fd8a645d82acc9395a5a3d637f473cc05df3c65
gergo-m <119097427+gergo-m@users.noreply.github.com>
2024-02-15 12:05:40 +0100

    initial commit
    added basic camera scanner

---
+++ shared/szksc-qr-code-attendance/script.js
@@ -0,0 +1,39 @@
+function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code
+    console.log(`scanned = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning
+    console.warn(`code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    { fps: 10, qrbox: {width: 250, height: 250} },
+    /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
+// -----------
+
+const html5QrCode = new Html5Qrcode(/* element id */ "reader");
+// file based scanning
+const fileinput = document.getElementById('qr-input-file');
+fileinput.addEventListener('change', e => {
+    if (e.target.files.length = 0) {
+        // no file selected, ignore
+        return;
+    }
+
+    const imageFile = e.target.files[0];
+    // scan QR code
+    html5QrCode.scanFile(imageFile, true)
+    .then(decodedText => {
+        // success, use decodedText
+        console.log(decodedText);
+    })
+    .catch(err => {
+        // failure, handle it
+        console.log(`error scanning file. Reason: ${err}`)
+    });
+});
